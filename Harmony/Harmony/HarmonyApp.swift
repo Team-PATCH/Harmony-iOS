4f42d52bdd052a869337bb2b8fce6cd2ed5af7ae
sion <hbs09222@gmail.com>
2024-07-15 09:58:04 +0900

    [Chore] 프로젝트 파일 생성

---
+++ Harmony/Harmony/HarmonyApp.swift
@@ -0,0 +1,17 @@
+//
+//  HarmonyApp.swift
+//  Harmony
+//
+//  Created by 한범석 on 7/15/24.
+//
+
+import SwiftUI
+
+@main
+struct HarmonyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
