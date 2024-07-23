a3680b0e144d1c8c3072c4b33e87fa2530fcbb43
sion <hbs09222@gmail.com>
2024-07-23 18:09:34 +0900

    [Feat] 추억카드 디자인 작업 및 등록 뷰 추가

--- Harmony/Harmony/Views/Home/WaveFormView.swift
+++ Harmony/Harmony/Views/Home/WaveFormView.swift
@@ -8,8 +8,33 @@
 import SwiftUI
 
 struct WaveFormView: View {
+    @State private var phase: CGFloat = 0
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        GeometryReader { geometry in
+            Path { path in
+                let width = geometry.size.width
+                let height = geometry.size.height
+                let midHeight = height / 2
+                let amplitude = height / 4
+
+                path.move(to: CGPoint(x: 0, y: midHeight))
+
+                for x in stride(from: 0, through: width, by: 5) {
+                    let relativeX = x / width
+                    let sine = sin(relativeX * 2 * .pi + phase)
+                    let y = midHeight + amplitude * sine
+
+                    path.addLine(to: CGPoint(x: x, y: y))
+                }
+            }
+            .stroke(Color.mainGreen, lineWidth: 2)
+            .onAppear {
+                withAnimation(Animation.linear(duration: 0.05).repeatForever(autoreverses: false)) {
+                    phase += .pi / 2
+                }
+            }
+        }
     }
 }
 
