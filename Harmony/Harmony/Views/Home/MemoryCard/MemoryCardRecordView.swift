1ad588631496bed833537f4d57d1173f26b9e9e9
sion <hbs09222@gmail.com>
2024-07-23 18:09:07 +0900

    [Feat] 추억카드 디자인 작업 및 등록 뷰 추가

---
+++ Harmony/Harmony/Views/Home/MemoryCard/MemoryCardRecordView.swift
@@ -0,0 +1,18 @@
+//
+//  MemoryCardRecordView.swift
+//  Harmony
+//
+//  Created by 한범석 on 7/23/24.
+//
+
+import SwiftUI
+
+struct MemoryCardRecordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MemoryCardRecordView()
+}
---
+++ Harmony/Harmony/Views/Home/WaveFormView.swift
@@ -0,0 +1,18 @@
+//
+//  WaveFormView.swift
+//  Harmony
+//
+//  Created by 한범석 on 7/23/24.
+//
+
+import SwiftUI
+
+struct WaveFormView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    WaveFormView()
+}
