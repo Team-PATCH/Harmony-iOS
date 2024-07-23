1b167f2f80cf7934debf0845fc16e18630be3fb1
thisisdana49 <thisisdana49@gmail.com>
2024-07-23 15:34:03 +0900

    [Feat] 더미 Routine  데이터로 DailyRoutine 생성 되도록 구현

--- Harmony/Harmony/ViewModels/RoutineViewModel.swift
+++ Harmony/Harmony/ViewModels/RoutineViewModel.swift
@@ -12,25 +12,39 @@ class RoutineViewModel: ObservableObject {
     @Published var routines: [Routine] = dummyRoutines
     @Published var dailyRoutines: [DailyRoutine] = []
     @Published var routineReactions: [RoutineReaction] = []
+    @Published var currentDateString: String = ""
+    @Published var currentDayString: String = ""
 
     var completionRate: Double {
         let completedCount = dailyRoutines.filter { $0.completedPhoto != nil }.count
         return Double(completedCount) / Double(dailyRoutines.count)
     }
-    
+
     init() {
+        updateCurrentDate()
         generateDailyRoutines()
+    }
+
+    func updateCurrentDate() {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy년 M월 d일 EEEE"
+        
+        let today = Date()
+        currentDateString = formatter.string(from: today)
+        currentDayString = formatter.weekdaySymbols[Calendar.current.component(.weekday, from: today) - 1]
     }
 
     func generateDailyRoutines() {
         let today = Date()
-        let calendar = Calendar.current
-        let weekday = calendar.component(.weekday, from: today) - 1
+        let calendar = Calendar(identifier: .gregorian)
+        let weekday = (calendar.component(.weekday, from: today) + 5) % 7 // 월요일 = 0, 화요일 = 1, ... 일요일 = 6
 
         dailyRoutines = routines.compactMap { routine in
-            if (routine.days & (1 << weekday)) != 0 {
+            let daysString = String(routine.days, radix: 2).pad(with: "0", toLength: 7)
+            if daysString[weekday] == "1" {
                 return DailyRoutine(
-                    id: routine.id,
+                    id: UUID().hashValue,
                     routineId: routine.id,
                     groupId: routine.groupId,
                     time: routine.time,
@@ -52,8 +66,9 @@ class RoutineViewModel: ObservableObject {
     func daysAsString(for routine: Routine) -> String {
         let daysArray = ["월", "화", "수", "목", "금", "토", "일"]
         var result = [String]()
+        let daysString = String(routine.days, radix: 2).pad(with: "0", toLength: 7)
         for (index, day) in daysArray.enumerated() {
-            if (routine.days & (1 << index)) != 0 {
+            if daysString[index] == "1" {
                 result.append(day)
             }
         }
@@ -61,7 +76,25 @@ class RoutineViewModel: ObservableObject {
     }
 }
 
+extension String {
+    func pad(with character: Character, toLength length: Int) -> String {
+        let padding = length - count
+        if padding > 0 {
+            return String(repeating: character, count: padding) + self
+        } else {
+            return self
+        }
+    }
+
+    subscript(i: Int) -> Character {
+        return self[index(startIndex, offsetBy: i)]
+    }
+}
+
+
 let dummyRoutines = [
-    Routine(id: 1, groupId: 1, title: "공원 산책", photo: URL(string: "https://example.com/photo1.jpg"), day: 0b01111111, time: Date(), createdAt: Date(), updatedAt: nil, deletedAt: nil), // 월화수목금토일
-    Routine(id: 3, groupId: 2, title: "요리하기", photo: URL(string: "https://example.com/photo3.jpg"), day: 0b00000001, time: Date(), createdAt: Date(), updatedAt: nil, deletedAt: nil)  // 월
+    Routine(id: 1, groupId: 1, title: "하율이 등원 시키기", photo: nil, days: 0b1111100, time: Date(), createdAt: Date(), updatedAt: nil, deletedAt: nil), // 월화수목금
+    Routine(id: 2, groupId: 1, title: "공원 산책가서 비둘기 사진 찍기", photo: nil, days: 0b1100000, time: Date(), createdAt: Date(), updatedAt: nil, deletedAt: nil), // 월화
+    Routine(id: 3, groupId: 1, title: "문화센터 서예 교실 가기", photo: URL(string: "https://example.com/photo2.jpg"), days: 0b0000011, time: Date(), createdAt: Date(), updatedAt: nil, deletedAt: nil), // 주말
+    Routine(id: 4, groupId: 1, title: "요리하기", photo: URL(string: "https://example.com/photo3.jpg"), days: 0b1000000, time: Date(), createdAt: Date(), updatedAt: nil, deletedAt: nil)  // 월
 ]
