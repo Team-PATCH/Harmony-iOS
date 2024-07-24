af79ee705c07bbcebd845b642df7cd71b9f750b9
WaterBean <gkstnqls9@gmail.com>
2024-07-24 20:17:39 +0900

    [Chore] 프로필 사진 등록 뷰 파일 생성

---
+++ Harmony/Harmony/Views/Onboarding/ProfileRegisterView.swift
@@ -0,0 +1,18 @@
+//
+//  ProfileRegisterView.swift
+//  Harmony
+//
+//  Created by 한수빈 on 7/24/24.
+//
+
+import SwiftUI
+
+struct ProfileRegisterView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ProfileRegisterView()
+}
